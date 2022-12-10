9f40792f1e53f99eb29bec4253bd0a504f88fddc
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2022-12-10 18:23:03 -0500

    refactor base app, start readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Demystifying React Hooks - useRef
 
+![](./assets/png/useRef-header-small.png)
+
 In this article, we will discuss some common use cases for React's `useRef` Hook.
+
+## Getting Started
+
+If you'd like to follow along in your local IDE, you can find the GitHub Repo [here](https://github.com/austin-rt/useRef).
+
+- `fork and clone`
+- `cd client`
+- `npm i`
+- `npm start`
+
+## Starter Code
+
+As always, we'll start with a tour of our current codebase. This time, `App.js` is a simulated Login Form. First, we have `initialFormValues`, an object with a blank email and password, which we use to initialize our `formValues` state.
+
+We then have boilerplate `handleChange` and `handleSubmit` functions. Our JSX renders the form and assigns the appropriate `onChange` and `onSubmit` attributes.
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,48 @@
-import './index.css';
-import Form from './components/Form';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const initialFormValues = {
+    email: '',
+    password: '',
+  };
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const handleChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+  };
+
+  useEffect(() => {
+    console.log('rendered');
+  }, [formValues]);
+
   return (
     <main>
-      <Form />
+      <section>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            email='email'
+            placeholder='Email'
+            onChange={handleChange}
+          />
+          <input
+            type='password'
+            password='password'
+            placeholder='Password'
+            onChange={handleChange}
+          />
+          <button>Login</button>
+        </form>
+      </section>
     </main>
   );
 }
